1dd58ea3f012dc4e9f3ef4903e155a25abb2a185
sarahmdgal <sarahrubin2@yahoo.com>
2017-08-25 16:56:59 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,75 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+         @RequestMapping("/login")
+        public String Login(Model model){
+        model.addAttribute("message", messageRepository.findAll());
+        return "login";
+    }
+
+
+        @RequestMapping("/")
+        public String index(){
+            return "messageform";
+        }
+        @RequestMapping("/list")
+        public String listMessages(Model model) {
+            model.addAttribute("message", messageRepository.findAll());
+            return "list";
+        }
+        @GetMapping("/add")
+        public String messageForm(Model model){
+        model.addAttribute("message", new Message());
+        return "messageform";
+
+     }
+
+
+     @PostMapping("/process")
+     public String processForm(@Valid Message message, BindingResult result){
+        if (result.hasErrors()){
+            return "messageform";
+        }
+        messageRepository.save(message);
+        return "redirect:/";
+     }
+
+     @RequestMapping("/detail/{id}")
+     public String showMessage(@PathVariable("id") long id, Model model){
+         model.addAttribute("message", messageRepository.findOne(id));
+         return "show";
+     }
+
+     @RequestMapping("/update/{id}")
+     public String updateMessage(@PathVariable("id") long id, Model model){
+         model.addAttribute("message", messageRepository.findOne(id));
+         return "messageform";
+     }
+
+     @RequestMapping("/delete/{id}")
+     public String delMessage(@PathVariable("id") long id){
+         messageRepository.delete(id);
+         return "redirect:/";
+     }
+}
